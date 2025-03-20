1017d62c2f5233bd606aaae5fac5ffc8e0f58c9d
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2025-03-20 10:03:11 -0400

    Made Vector Draw Method Return Kinematics Vector

--- VectorDraw.gd
+++ VectorDraw.gd
@@ -4,4 +4,20 @@ func _ready() -> void:
 	print(cartesian_to_kinematics(Vector2(100,100)))
 
 func cartesian_to_kinematics(vec : Vector2):
-	return str(round(vec.length()))+"m ["+str(round(rad_to_deg(vec.angle())))+"\u00B0]"
+	var dir = round(rad_to_deg(vec.angle()))
+	if dir == 0:
+		return str(round(vec.length())+"m [W]")
+	elif 90 > dir and dir > 0:
+		return str(round(vec.length()))+"m [W "+str(dir)+"\u00B0 N]"
+	elif dir == 90:
+		return str(round(vec.length())+"m [N]")
+	elif 180 > dir and dir > 90:
+		return str(round(vec.length()))+"m [N "+str(dir-90)+"\u00B0 E]"
+	elif dir == 180:
+		return str(round(vec.length())+"m [E]")
+	elif 270 > dir and dir > 180:
+		return str(round(vec.length()))+"m [W "+str(dir)+"\u00B0 N]"
+	elif dir == 270:
+		return str(round(vec.length())+"m [S]")
+	elif 360 > dir and dir > 90:
+		return str(round(vec.length()))+"m [N "+str(dir-90)+"\u00B0 E]"
