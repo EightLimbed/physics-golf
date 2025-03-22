0758413e53a3414296fbbead7804fa0f9c9ea14a
Dryden <drydenellis@gmail.com>
2025-03-21 20:55:22 -0400

    Finished Launching, Fixed Vector Conversion

--- Ball.gd
+++ Ball.gd
@@ -3,26 +3,54 @@ extends RigidBody2D
 @onready var game = get_parent()
 var launch_ready : bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game.vectors.append(position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
-	if Input.is_action_just_released("Launch") and launch_ready:
-		apply_central_impulse(-get_local_mouse_position()*2)
-		launch_ready = false
+	if launch_ready:
+		queue_redraw()
+		$Label.show()
+		$Sprite2D2.show()
+		$Sprite2D2.position = -get_local_mouse_position()
+		$Sprite2D2.rotation = get_local_mouse_position().angle()-PI/2
+		$Label.text = cartesian_to_kinematics(-get_local_mouse_position())
+	else:
+		$Label.hide()
+		$Sprite2D2.hide()
 
 func _on_body_entered(_body: Node) -> void:
 	game.vectors.append(position)
 	print(game.vectors)
 
-func _on_mouse_entered() -> void:
-	if Input.is_action_pressed("Launch"):
-		print("entered")
-		launch_ready = true
-
 func _draw() -> void:
 	if launch_ready:
-		draw_line(position, get_local_mouse_position(), Color.YELLOW, 6, true)
+		draw_line(Vector2.ZERO, -get_local_mouse_position(), Color.YELLOW, 6, true)
+
+func cartesian_to_kinematics(df : Vector2):
+	var dir = rad_to_deg(df.angle())
+	if dir < 0:
+		dir += 360
+	if round(dir) == 0:
+		return str(round(df.length()/100))+"m/s [W]"
+	elif 90 > dir and dir > 0:
+		return str(round(df.length()/100))+"m/s [E "+str(round(dir))+"\u00B0 S]"
+	elif round(dir) == 90:
+		return str(round(df.length()/100))+"m/s [S]"
+	elif 180 > dir and dir > 90:
+		return str(round(df.length()/100))+"m/s [S "+str(round(dir-90))+"\u00B0 W]"
+	elif round(dir) == 180:
+		return str(round(df.length()/100))+"m/s [E]"
+	elif 270 > dir and dir > 180:
+		return str(round(df.length()/100))+"m/s [W "+str(round(dir-180))+"\u00B0 N]"
+	elif round(dir) == 270:
+		return str(round(df.length()/100))+"m/s [N]"
+	elif 360 > dir and dir > 270:
+		return str(round(df.length()/100))+"m/s [N "+str(round(dir-270))+"\u00B0 E]"
+
+func _on_texture_button_button_down() -> void:
+	launch_ready = true
+
+func _on_texture_button_button_up() -> void:
+	apply_central_impulse(-get_local_mouse_position()*2)
+	launch_ready = false
+	queue_redraw()
--- Game.gd
+++ Game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var vectors : Array[Vector2] = []
+var vectors_kinematics : Array[String] = []
 @onready var ball = $Ball
 
 func _input(event: InputEvent) -> void:
@@ -12,35 +13,40 @@ func _input(event: InputEvent) -> void:
 		queue_redraw()
 
 func display_vector(d1 : Vector2,d2 : Vector2):
-	var label = Label.new()
-	label.text = cartesian_to_kinematics(d1+d2)
-	label.global_position = (d1+d2)/2
-	add_child(label)
+	vectors_kinematics.append(cartesian_to_kinematics(d1+d2))
+	#var label = Label.new()
+	#label.text = cartesian_to_kinematics(d1+d2)
+	#label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	#add_child(label)
 
 func cartesian_to_kinematics(df : Vector2):
 	var dir = rad_to_deg(df.angle())
 	if dir < 0:
 		dir += 360
-	if dir == 0:
+	if round(dir) == 0:
 		return str(round(df.length()/100))+"m [W]"
 	elif 90 > dir and dir > 0:
-		return str(round(df.length()/100))+"m [W "+str(dir)+"\u00B0 N]"
-	elif dir == 90:
-		return str(round(df.length()/100))+"m [N]"
+		return str(round(df.length()/100))+"m [E "+str(round(dir))+"\u00B0 S]"
+	elif round(dir) == 90:
+		return str(round(df.length()/100))+"m [S]"
 	elif 180 > dir and dir > 90:
-		return str(round(df.length()/100))+"m [N "+str(dir-90)+"\u00B0 E]"
-	elif dir == 180:
+		return str(round(df.length()/100))+"m [S "+str(round(dir-90))+"\u00B0 W]"
+	elif round(dir) == 180:
 		return str(round(df.length()/100))+"m [E]"
 	elif 270 > dir and dir > 180:
-		return str(round(df.length()/100))+"m [W "+str(dir)+"\u00B0 N]"
-	elif dir == 270:
-		return str(round(df.length()/100))+"m [S]"
+		return str(round(df.length()/100))+"m [W "+str(round(dir-180))+"\u00B0 N]"
+	elif round(dir) == 270:
+		return str(round(df.length()/100))+"m [N]"
 	elif 360 > dir and dir > 270:
-		return str(round(df.length()/100))+"m [N "+str(dir-90)+"\u00B0 E]"
+		return str(round(df.length()/100))+"m [N "+str(round(dir-270))+"\u00B0 E]"
 
 func _draw() -> void:
 	vectors.append(ball.position)
 	if vectors.size() > 1:
-		draw_polyline(vectors, Color.AQUAMARINE, 2, true)
+		for i in vectors.size()-1:
+			if round(i/2.0) == i/2.0:
+				draw_line(vectors[i], vectors[i+1], Color.AQUAMARINE, 2, true)
+			else:
+				draw_line(vectors[i], vectors[i+1], Color.GREEN, 2, true)
 		draw_line(vectors[0],ball.position, Color.BLUE, 4, true)
 	vectors.remove_at(vectors.size()-1)
